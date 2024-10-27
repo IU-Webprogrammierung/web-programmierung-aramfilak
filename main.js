5d07411c9f38d8c4d6a2962975e1aaa5ea5d6662
aramfilak <aram.filak@outlook.de>
2024-10-27 10:14:21 +0100

    refactor formatting and add prettier configuration

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // animate the cursor by add radial-gradient to the cursor
-document.querySelectorAll("section").forEach((section) => {
-  section.addEventListener("mousemove", (e) => {
+document.querySelectorAll('section').forEach((section) => {
+  section.addEventListener('mousemove', (e) => {
     const rect = section.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
-    section.style.setProperty("--x", `${x}px`);
-    section.style.setProperty("--y", `${y}px`);
+    section.style.setProperty('--x', `${x}px`);
+    section.style.setProperty('--y', `${y}px`);
   });
 });
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        index: resolve(__dirname, "index.html"),
-        education: resolve(__dirname, "education.html"),
-        experience: resolve(__dirname, "experience.html"),
-        projects: resolve(__dirname, "projects.html"),
+        index: resolve(__dirname, 'index.html'),
+        education: resolve(__dirname, 'education.html'),
+        experience: resolve(__dirname, 'experience.html'),
+        projects: resolve(__dirname, 'projects.html'),
       },
     },
   },
